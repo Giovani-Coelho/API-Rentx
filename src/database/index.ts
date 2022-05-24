a0ef97fa8bbe17fa3494bfa0d8b753cae7c8a679
Giovani Coelho <giovanicoelho2004@hotmail.com>
2022-05-24 18:02:43 -0300

    commit-2

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,13 @@
-import { createConnection } from 'typeorm'
-// para fazer uma conexao
-createConnection()
+import { createConnection, getConnectionOptions } from 'typeorm'
+
+interface IOptions {
+  host: string
+}
+
+getConnectionOptions().then(options => {
+  const newOptions = options as IOptions
+  newOptions.host = 'database' // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+  createConnection({
+    ...options,
+  })
+})
