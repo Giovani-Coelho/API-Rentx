ade7ebb0d6221bf5a56d112afc663ae3442e4d48
Giovani Coelho <giovanicoelho2004@hotmail.com>
2022-05-25 13:31:02 -0300

    commit error

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,8 +2,8 @@ import { Router } from 'express'
 import multer from 'multer'
 
 import createCategoryController from '../modules/cars/useCases/createCategory'
-import { importCategoryController } from '../modules/cars/useCases/importCategory'
-import { listCategoriesController } from '../modules/cars/useCases/listCategories'
+import importCategoryController from '../modules/cars/useCases/importCategory'
+import listCategoriesController from '../modules/cars/useCases/listCategories'
 
 const categoriesRoutes = Router()
 // passar qual o destino onde vai ser salvo, usa-lo como middleware
@@ -17,11 +17,11 @@ categoriesRoutes.post('/', (req, res) =>
 )
 // responsavel por pegar e listar os objetos do banco de dados
 categoriesRoutes.get('/', (req, res) =>
-  listCategoriesController.handle(req, res),
+  listCategoriesController().handle(req, res),
 )
 // passa o middleware e falar que vai ser importado apenas um arquivo e passado o nome do arquivo
 categoriesRoutes.post('/import', upload.single('file'), (req, res) =>
-  importCategoryController.handle(req, res),
+  importCategoryController().handle(req, res),
 )
 
 export { categoriesRoutes }
