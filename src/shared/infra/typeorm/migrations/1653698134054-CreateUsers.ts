1e56c0f0fb5e280293fe83f8acd87757c778a78f
Giovani Coelho <giovanicoelho2004@hotmail.com>
2022-07-14 23:19:35 -0300

    commit rentals

--- src/shared/infra/typeorm/migrations/1653698134054-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1653698134054-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1653698134054 implements MigrationInterface {
           {
             name: 'id',
             type: 'uuid',
+            isPrimary: true,
           },
           {
             name: 'name',
